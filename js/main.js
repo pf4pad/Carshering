94cee5423cfb3ffbc2669fd8647060c2dc817848
pf4pad <65763670+pf4pad@users.noreply.github.com>
2021-02-12 14:30:59 +0200

    footer

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 $(function(){
 $('.top__slider').slick({
-arrows:  false,
-dots: true,
-autoplay: true,
-fade:true
+  arrows:  false,
+  dots: true,
+  autoplay: true,
+  fade:true
 
+});
+});
+$(function(){
+$('.reviews__slider').slick({
+  arrows:  false,
+  dots: true,
+  autoplay: true,
+  slidesToShow: 4,
+  slidesToScroll: 1
 });
 });
