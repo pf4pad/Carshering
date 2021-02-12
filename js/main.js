0587304c084e5803b42781659b11459818f10621
pf4pad <65763670+pf4pad@users.noreply.github.com>
2021-02-12 23:38:43 +0200

    all

--- js/main.js
+++ js/main.js
@@ -3,7 +3,30 @@ $('.top__slider').slick({
   arrows:  false,
   dots: true,
   autoplay: true,
-  fade:true
+  fade:true,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    
+  ]
 
 });
 
@@ -13,7 +36,30 @@ $('.reviews__slider').slick({
   dots: true,
   autoplay: true,
   slidesToShow: 4,
-  slidesToScroll: 1
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1141,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 846,
+      settings: {
+        slidesToShow: 2,
+        
+      }
+    },
+    {
+      breakpoint: 586,
+      settings: {
+        slidesToShow: 1,
+        
+      }
+    }
+    
+  ]
 });
 
 $('.menu__btn').on('click', function(){
